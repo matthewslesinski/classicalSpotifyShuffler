899c3141be61d3e49647a9a04dc79fdf5adce306
morcanie <mslesinski12@gmail.com>
2021-11-01 16:56:19 -0400

    Including files that should've been included already

---
+++ SpotifyProject/Configuration/SpotifySettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ApplicationResources.Setup;
+using CustomResources.Utils.GeneralUtils;
+using static ApplicationResources.Setup.CommandLineSettingsProvider;
+
+namespace SpotifyProject.Configuration
+{
+	[EnumExtensionProvider(typeof(SpotifySettingsSpecifications))]
+	[EnumExtensionProvider(typeof(SpotifySettingsCommandLineSpecifications))]
+	public enum SpotifySettings
+	{
+		TrackQueueSizeLimit,
+		AskUser,
+		HTTPLoggerName,
+		MetadataRecordFile,
+		NumHTTPConnections,
+		HTTPLoggerCharacterLimit,
+	}
+
+	public class SpotifySettingsSpecifications : IEnumExtensionProvider<SpotifySettings, ISettingSpecification>
+	{
+		public IReadOnlyDictionary<SpotifySettings, ISettingSpecification> Specifications { get; } = new Dictionary<SpotifySettings, ISettingSpecification>
+		{
+			{ SpotifySettings.MetadataRecordFile,                new StringSettingSpecification() },
+			{ SpotifySettings.HTTPLoggerName,                    new StringSettingSpecification() },
+			{ SpotifySettings.AskUser,                           new BoolSettingSpecification() },
+			{ SpotifySettings.TrackQueueSizeLimit,               new ConvertibleSettingSpecification<int> { Default = 750} },
+			{ SpotifySettings.NumHTTPConnections,                new ConvertibleSettingSpecification<int> { ValueGetter = values => int.TryParse(values.Single(), out var numConnections) && numConnections > 0 ? numConnections : int.MaxValue, Default = int.MaxValue } },
+			{ SpotifySettings.HTTPLoggerCharacterLimit,          new NullableConvertibleSettingSpecification<int> { ValueGetter = values => int.TryParse(values.Single(), out var characterLimit) && characterLimit > 0 ? characterLimit : null, Default = null } },
+		};
+	}
+
+	public class SpotifySettingsCommandLineSpecifications : IEnumExtensionProvider<SpotifySettings, ICommandLineSpecification>
+	{
+		public IReadOnlyDictionary<SpotifySettings, ICommandLineSpecification> Specifications { get; } = new Dictionary<SpotifySettings, ICommandLineSpecification>
+		{
+			{ SpotifySettings.TrackQueueSizeLimit,             new SingleValueOption { Flag = "-q|--queueSizeLimit", Desc = "The cap on the number of tracks to send in a request to create a new queue" } },
+			{ SpotifySettings.AskUser,                         new NoValueOption     { Flag = "--askUser", Desc = "Provide if the user should be asked what context to reorder" } },
+			{ SpotifySettings.HTTPLoggerName,                  new SingleValueOption { Flag = "--httpLogger", Desc = "The name of the http logger to be used." } },
+			{ SpotifySettings.HTTPLoggerCharacterLimit,        new SingleValueOption { Flag = "--httpLoggerCharacterLimit", Desc = "The max number of characters to print per line from the http logger." } },
+			{ SpotifySettings.MetadataRecordFile,              new SingleValueOption { Flag = "--metadataRecordFile", Desc = "The location to write input to LukesTrackLinker for unit tests" } },
+			{ SpotifySettings.NumHTTPConnections,              new SingleValueOption { Flag = "--numHttpConnections", Desc = "The number of http connections to spotify's api to allow" } }
+		};
+	}
+}
