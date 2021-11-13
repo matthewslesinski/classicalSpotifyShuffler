d7648c6da5be3283c2c477770191988461a97a06
morcanie <mslesinski12@gmail.com>
2021-11-12 22:24:09 -0500

    Taking advantage of changes in SpotifyAPI dependency and adding support for cancellation tokens to api connector

--- SpotifyProject/SpotifyAdditions/ModifiedAPIConnector.cs
+++ SpotifyProject/SpotifyAdditions/ModifiedAPIConnector.cs
@@ -10,64 +10,33 @@ using System.IO;
 
 namespace SpotifyProject.SpotifyAdditions
 {
-	public class ModifiedAPIConnector : ModifiedAPIConnectorBase
+	public class ModifiedAPIConnector : APIConnector
 	{
 		private static int _requestsSent = 0;
 
-		private readonly IHTTPClient _httpClient;
-
-		private readonly IRetryHandler _retryHandler;
-
-		private readonly IHTTPLogger _httpLogger;
-
-		public override event EventHandler<IResponse> ResponseReceived;
-
 		public ModifiedAPIConnector(Uri baseAddress, IAuthenticator authenticator)
-			: this(baseAddress, authenticator, new NewtonsoftJSONSerializer(), new NetHttpClient(), null, null)
+			: base(baseAddress, authenticator)
 		{
 		}
 
 		public ModifiedAPIConnector(Uri baseAddress, IAuthenticator authenticator, IJSONSerializer jsonSerializer, IHTTPClient httpClient, IRetryHandler retryHandler, IHTTPLogger httpLogger)
-			: base(baseAddress, authenticator, jsonSerializer)
-		{
-			_httpClient = httpClient;
-			_retryHandler = retryHandler;
-			_httpLogger = httpLogger;
-		}
+			: base(baseAddress, authenticator, jsonSerializer, httpClient, retryHandler, httpLogger)
+		{ }
 
-		public override void SetRequestTimeout(TimeSpan timeout)
-		{
-			_httpClient.SetRequestTimeout(timeout);
-		}
-
-		protected override async Task<IResponse> DoRequest(IRequest request)
+		protected override Task<IResponse> DoRequest(IRequest request, CancellationToken? cancellationToken = null)
 		{
 			var requestNumber = Interlocked.Increment(ref _requestsSent);
-			using (Loggers.HTTPLogger.BeginScope($"Request ID: {requestNumber}")) {
-				IRequest request2 = request;
-				IResponse response = await SendRequest(request2);
-				if (_retryHandler != null)
-					response = await _retryHandler!.HandleRetry(request2, response, SendRequest).WithoutContextCapture();
-				
-				ProcessErrors(response);
-				return response;
-			}
-		}
-		private async Task<IResponse> SendRequest(IRequest request) {
-			await ApplyAuthenticator(request).WithoutContextCapture();
-			_httpLogger?.OnRequest(request);
-			IResponse response;
-			try
+			using (Loggers.HTTPLogger.BeginScope($"Request ID: {requestNumber}"))
 			{
-				response = await _httpClient.DoRequest(request).WithoutContextCapture();
+				try
+				{
+					return base.DoRequest(request, cancellationToken);
+				}
+				catch (HttpRequestException e) when (e.InnerException is IOException ioE && ioE.Message.Contains("Received an unexpected EOF or 0 bytes from the transport stream."))
+				{
+					throw new APIException("The Spotify server did not send a response", e);
+				}
 			}
-			catch (HttpRequestException e) when (e.InnerException is IOException ioE && ioE.Message.Contains("Received an unexpected EOF or 0 bytes from the transport stream."))
-			{
-				throw new APIException("The Spotify server did not send a response", e);
-			}
-			_httpLogger?.OnResponse(response);
-			this.ResponseReceived?.Invoke(this, response);
-			return response;
 		}
 	}
 }
