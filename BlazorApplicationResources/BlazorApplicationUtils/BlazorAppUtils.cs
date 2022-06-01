59df71295e91e42b5cf95830f4bc8a959319bd38
matthewslesinski <mslesinski12@gmail.com>
2022-06-01 14:05:21 -0400

    Adding initial blazor resources

---
+++ BlazorApplicationResources/BlazorApplicationUtils/BlazorAppUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ApplicationResources.ApplicationUtils;
+using ApplicationResources.Logging;
+using ApplicationResources.Services;
+using Blazored.LocalStorage;
+using ClassicalSpotifyShuffler.Utils;
+using CustomResources.Utils.Extensions;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using static ApplicationResources.ApplicationUtils.ProgramUtils;
+
+namespace BlazorApplicationResources.BlazorApplicationUtils
+{
+	public static class BlazorAppUtils
+	{
+		public static async Task StartApp(Func<WebAssemblyHostBuilder, Task> hostSetup, Func<Task> postSetup, StartupArgs startupArgs, Func<IDisposable>? dependencyInitializer = null)
+		{
+			var hostBuilder = WebAssemblyHostBuilder.CreateDefault(startupArgs.CommandLineArgs);
+
+			hostBuilder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(hostBuilder.HostEnvironment.BaseAddress) });
+			hostBuilder.Services.AddBlazoredLocalStorage();
+			hostBuilder.Services.AddSingleton<IDataStoreAccessor, LocalStorageAccessor>();
+
+			await hostSetup(hostBuilder).WithoutContextCapture();
+			var host = hostBuilder.Build();
+
+			await ProgramUtils.ExecuteProgram(async () =>
+			{
+				Console.WriteLine("Starting");
+				Logger.Information("Starting");
+
+				Console.WriteLine($"Base address is {hostBuilder.HostEnvironment.BaseAddress}");
+				await postSetup().WithoutContextCapture();
+				await host.RunAsync();
+			}, startupArgs, () => GlobalDependencies.InitializeWith(host.Services)).WithoutContextCapture();
+		}
+	}
+}
---
+++ BlazorApplicationResources/Services/LocalStorageAccessor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ApplicationResources.Services;
+using Blazored.LocalStorage;
+using CustomResources.Utils.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClassicalSpotifyShuffler.Utils
+{
+	public class LocalStorageAccessor : IDataStoreAccessor
+	{
+		private static ILocalStorageService BrowserLocalStorage => GlobalDependencies.GlobalDependencyContainer.GetRequiredService<ILocalStorageService>();
+		private static HttpClient Server => GlobalDependencies.GlobalDependencyContainer.GetRequiredService<HttpClient>();
+		public LocalStorageAccessor()
+		{
+		}
+
+		public async Task<bool> ExistsAsync(string key, CancellationToken cancellationToken)
+		{
+			var localStorageTask = BrowserLocalStorage.ContainKeyAsync(key, cancellationToken).WithoutContextCapture();
+			var serverTask = Server.GetStringAsync(key, cancellationToken).WithoutContextCapture();
+			return await localStorageTask || await serverTask != null;
+		}
+
+		public async Task<string?> GetAsync(string key, CancellationToken cancellationToken)
+		{
+			// TODO Make this work better, and cache files in LocalStorage
+			var serverValue = await Server.GetStringAsync(key, cancellationToken).WithoutContextCapture();
+			if (serverValue != null)
+				return serverValue;
+			if (await BrowserLocalStorage.ContainKeyAsync(key, cancellationToken).WithoutContextCapture())
+				return await BrowserLocalStorage.GetItemAsStringAsync(key, cancellationToken).WithoutContextCapture();
+			return default;
+		}
+
+		public async Task<bool> SaveAsync(string key, string data, CancellationToken cancellationToken)
+		{
+			await BrowserLocalStorage.SetItemAsStringAsync(key, data, cancellationToken).AsTask().WithoutContextCapture();
+			return true;
+		}
+
+		async Task<(bool foundData, string? data)> IDataStoreAccessor.TryGetAsync(string key, CancellationToken cancellationToken)
+		{
+			var getTask = GetAsync(key, cancellationToken).WithoutContextCapture();
+			var data = await getTask;
+			return (data != null, data);
+		}
+	}
+}
